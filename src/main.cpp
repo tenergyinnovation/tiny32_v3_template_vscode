47cdf6adde983dd106d8655d229bd3fdc712b722
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-07-04 17:31:18 +0700

    Rev1.0
    Template coding for tiny32_v3 on vscode with platformIO

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+/***********************************************************************
+ * Project      :     tiny32_v3_temploate
+ * Description  :     Template coding for tiny32_v3 on vscode with platformIO
+ * Hardware     :     tiny32_v3         
+ * Author       :     Tenergy Innovation Co., Ltd.
+ * Date         :     04/07/2022
+ * Revision     :     1.0
+ * Rev1.0       :     Origital
+ * website      :     http://www.tenergyinnovation.co.th
+ * Email        :     admin@innovation.co.th
+ * TEL          :     +66 82-380-3299
+ ***********************************************************************/
+#include <Arduino.h>
+#include <tiny32_v3.h>
+
+/**************************************/
+/*          Firmware Version          */
+/**************************************/
+String version = "0.1";
+
+/**************************************/
+/*        define object variable      */
+/**************************************/
+tiny32_v3 mcu;
+
+/**************************************/
+/*            GPIO define             */
+/**************************************/
+
+
+/**************************************/
+/*       Constand define value        */
+/**************************************/
+
+/**************************************/
+/*       eeprom address define        */
+/**************************************/
+
+/**************************************/
+/*        define global variable      */
+/**************************************/
+
+/**************************************/
+/*           define function          */
+/**************************************/
+
+
+/***********************************************************************
+ * FUNCTION:    setup
+ * DESCRIPTION: setup process
+ * PARAMETERS:  nothing
+ * RETURNED:    nothing
+ ***********************************************************************/
+void setup()
+{
+ Serial.begin(115200);
+ Serial.printf("Hello World\r\n");
+
+}
+
+ /***********************************************************************
+ * FUNCTION:    loop
+ * DESCRIPTION: loop process
+ * PARAMETERS:  nothing
+ * RETURNED:    nothing
+ ***********************************************************************/
+void loop()
+{
+
+
+}
