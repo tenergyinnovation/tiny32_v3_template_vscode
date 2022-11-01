1409b9d66f026c0bde456fc42b5c9119efb5ec24
Tenergy Innovation <45764087+tenergyinnovation@users.noreply.github.com>
2022-11-01 21:04:18 +0700

    update code

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,25 @@
 String version = "0.1";
 
 /**************************************/
+/*          Header project            */
+/**************************************/
+void header_print(void)
+{
+Serial.printf("\r\n***********************************************************************\r\n");
+Serial.printf("* Project      :     tiny32_v3_template\r\n");
+Serial.printf("* Description  :     Template coding for tiny32_v3 on vscode with platformIO\r\n");
+Serial.printf("* Hardware     :     tiny32_v3\r\n");
+Serial.printf("* Author       :     Tenergy Innovation Co., Ltd.\r\n");
+Serial.printf("* Date         :     04/07/2022\r\n");
+Serial.printf("* Revision     :     %s\r\n",version);
+Serial.printf("* Rev1.0       :     Origital\r\n");
+Serial.printf("* website      :     http://www.tenergyinnovation.co.th\r\n");
+Serial.printf("* Email        :     admin@innovation.co.th\r\n");
+Serial.printf("* TEL          :     +66 82-380-3299\r\n");
+Serial.printf("***********************************************************************/\r\n");
+}
+
+/**************************************/
 /*        define object variable      */
 /**************************************/
 tiny32_v3 mcu;
@@ -54,7 +73,7 @@ tiny32_v3 mcu;
 void setup()
 {
  Serial.begin(115200);
- Serial.printf("Hello World\r\n");
+ header_print();
 
 }
 
